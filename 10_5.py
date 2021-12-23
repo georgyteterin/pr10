e6d9e00aa09407cb938ac83c1d5a86d5271def7a
georgyteterin <teterin.normal@gmail,com>
2021-12-23 16:18:10 +0300

    adding tasks

---
+++ 10_5.py
@@ -0,0 +1,8 @@
+fib_dic = {1: 1, 2: 1}
+
+def fib_mem(n):
+    znach = fib_dic.get(n)
+    while znach == None:
+        znach = fib_mem(n-2) + fib_mem(n-1)
+        fib_dic[n] = znach
+    return znach
